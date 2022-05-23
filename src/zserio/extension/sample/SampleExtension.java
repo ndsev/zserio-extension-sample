6a3764ca91425851b923934c9d6d98b0a3627adc
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2022-05-23 09:47:35 +0200

    Change expected zserio core version to 2.6.1

--- src/zserio/extension/sample/SampleExtension.java
+++ src/zserio/extension/sample/SampleExtension.java
@@ -54,6 +54,6 @@ public class SampleExtension implements Extension
     }
 
     private final static String EXTENSION_VERSION_STRING = "0.1.0";
-    private final static String ZSERIO_VERSION_STRING = "2.6.0";
+    private final static String ZSERIO_VERSION_STRING = "2.6.1";
     private final static String OPTION_SAMPLE = "sample";
 }
